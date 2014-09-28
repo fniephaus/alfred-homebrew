63e9166e175d64feebdd54363cab2bffd3974581
Fabio Niephaus <fniephaus@users.noreply.github.com>
2014-09-28 19:02:39 +0200

    Security improvements

--- src/cask_refresh.py
+++ src/cask_refresh.py
@@ -1,22 +1,20 @@
-import os
+import subprocess
 from workflow import Workflow, PasswordNotFound
 
 
 def get_all_casks():
-    return os.popen('/usr/local/bin/brew cask search').readlines()[1:]
+    cmd, err = subprocess.Popen(
+        ['/usr/local/bin/brew', 'cask', 'search'], stdout=subprocess.PIPE).communicate()
+    return cmd.splitlines()[1:]
 
 
 def get_installed_casks():
-    return os.popen('/usr/local/bin/brew cask list').readlines()
+    cmd, err = subprocess.Popen(
+        ['/usr/local/bin/brew', 'cask', 'list'], stdout=subprocess.PIPE).communicate()
+    return cmd.splitlines()
 
-
-def get_alfred_status():
-    return os.popen('/usr/local/bin/brew cask alfred status').readlines()[0].split('\n')[0]
 
 if __name__ == '__main__':
     wf = Workflow()
     wf.cache_data('cask_all', get_all_casks())
     wf.cache_data('cask_installed', get_installed_casks())
-    wf.cache_data('cask_alfred_status', get_alfred_status())
-
-    print get_installed_casks()
