a2b0aaa8fd65c23888248af142bce34eb9f736e7
abbasegbeyemi <abbasegbeyemi@gmail.com>
2020-12-30 13:15:24 +0000

    - helpers: Added a function to initialise the path and include path to the directory recommended by homebrew to install the native arm version of homebrew. If ARM brew is installed, it places it first in the path, which means that alfred will default to arm brew for the brew and cask commands. Added a dictionary containing default paths to intel and ARM homebrew installations. Added logic to the function that checks if brew is installed to check for both INTEL and ARM version installations. - cask.py and brew.py Changed the environment initialiser to use the helper defined above. - tests.py Refactored the unit tests to account for changed to the functions introduced in the last update (5.2)

--- src/tests.py
+++ src/tests.py
@@ -9,12 +9,11 @@ class HomeBrewTestCase(unittest.TestCase):
 
     def setUp(self):
         self.wf = Workflow()
+        cask.wf = Workflow()
 
-    def test_get_all_packages(self):
-        result = brew.get_all_packages(self.wf, '')
+    def test_get_all_formulae(self):
+        result = brew.get_all_formulae()
         self.assertTrue(len(result) > 0)
-        result = brew.get_all_packages(self.wf, '------ ------')
-        self.assertTrue(len(result) == 0)
 
     def test_search_key_for_action(self):
         result = helpers.search_key_for_action({
@@ -23,12 +22,8 @@ class HomeBrewTestCase(unittest.TestCase):
         })
         self.assertEquals(result, u'a b')
 
-    def test_brew_get_all_packages(self):
-        result = brew.get_all_packages(self.wf, '')
-        self.assertTrue(len(result) > 0)
-
     def test_brew_get_installed_packages(self):
-        result = brew.get_installed_packages(self.wf, '')
+        result = brew.get_installed_formulae()
         self.assertTrue(len(result) >= 0)
 
     def test_brew_get_info(self):
@@ -36,18 +31,21 @@ class HomeBrewTestCase(unittest.TestCase):
         self.assertTrue(all(x in result for x in ['kegs', 'files']))
 
     def test_cask_get_all_casks(self):
-        result = cask.get_all_casks(self.wf, '')
+        result = cask.get_all_casks()
         self.assertTrue(len(result) > 0)
 
     def test_cask_get_installed_casks(self):
-        result = cask.get_installed_casks(self.wf, '')
+        result = cask.get_installed_casks()
         self.assertTrue(len(result) >= 0)
 
     def test_cask_execute(self):
-        result = cask.execute(self.wf, 'notimplemented')
-        self.assertIsNone(result)
-        for cmd in ['search', 'list', 'alfred status']:
-            result = cask.execute(self.wf, cmd)
+        for cmd in ['search', 'list']:
+            result = cask.execute(self.wf, ['brew', cmd, '--cask'])
+            self.assertTrue(len(result) > 0)
+
+    def test_brew_execute(self):
+        for cmd in ['search', 'list']:
+            result = cask.execute(self.wf, ['brew', cmd])
             self.assertTrue(len(result) > 0)
 
 
